26fd28967d623b9406b9d7f864f1d34aacb0d83e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-08-02 14:53:00 +0900

    Change: Paml\Parser\parse() throws ParseException.

---
+++ src/HTML/Paml/Exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Paml: PHP array markup language.
+ *
+ * @author Yuya Takeyama
+ */
+
+namespace HTML\Paml;
+
+/**
+ * Exception class.
+ *
+ * @author Yuya Takeyama
+ */
+class Exception extends \Exception
+{
+}
