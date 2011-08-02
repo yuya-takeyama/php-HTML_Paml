ff6ac71e1b59fcb4dfd32e62fde2d9ea9a252efc
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-08-02 15:04:03 +0900

    Add: Files to require.

--- src/HTML/Paml.php
+++ src/HTML/Paml.php
@@ -12,6 +12,8 @@ require_once __DIR__ . '/Paml/Element.php';
 require_once __DIR__ . '/Paml/TextNode.php';
 require_once __DIR__ . '/Paml/Parser.php';
 require_once __DIR__ . '/Paml/Util.php';
+require_once __DIR__ . '/Paml/Exception.php';
+require_once __DIR__ . '/Paml/Exception/ParseException.php';
 
 class Paml
 {
